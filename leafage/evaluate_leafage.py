554b63bd9b17bae584d449d7c797175decab281c
Adhikari <ajaya.adhikari@tno.nl>
2018-08-03 16:13:48 +0200

    visualization of examples in tables

--- leafage/evaluate_leafage.py
+++ leafage/evaluate_leafage.py
@@ -12,7 +12,11 @@ def housing_from_use_cases():
     scenario = Scenario("load_from_use_cases", "housing", "lr")
     leafage = scenario.leafage
     explanation = scenario.get_explanation(leafage.training_data.feature_vector[0], 5)
-    explanation.visualize_feature_importance_png("../output/test.png")
+    explanation.visualize_feature_importance(target="write_to_file", path="../output/feature_importance.png")
+    explanation.visualize_examples(target="write_to_file", path="../output/examples_in_support.png", type="examples_in_support")
+    explanation.visualize_examples(target="write_to_file", path="../output/examples_against.png", type="examples_against")
+
+
     # c.evaluation.plot()
     # c.explanatory_examples.visualize(c.test[0])
 
--- leafage/explanation.py
+++ leafage/explanation.py
@@ -26,6 +26,50 @@ class Explanation:
         self.local_model = local_model
         self.__sort_columns_according_to_importance()
 
+    def visualize_feature_importance(self, amount_of_features=5, target="notebook", path=None):
+        """
+        Visualize a bar plot which contains the most important features for the classification of self.test_instance
+        :param amount_of_features: Amount of top features to include in the bar plot
+        :param target: Should be either "notebook" or "write_to_file"
+        :param path: If target="to_file", this parameter denotes where to save the image e.g. "../output/test.png"
+        """
+        figure = self.__visualize_feature_importance(amount_of_features)
+        self.__export(figure, target, path)
+
+    def visualize_examples(self, amount_of_features=5, target="notebook", path=None, type="examples_in_support"):
+        """
+        Visualize a table of closest examples from the training-set
+        :param amount_of_features:
+        :param target: Denotes how to export the image. Should be either "notebook" or "write_to_file"
+        :param path: If target="to_file", this parameter denotes where to save the image e.g. "../output/test.png"
+        :param type: Should be either "examples_in_support" or "examples_against"
+        """
+        if type == "examples_in_support":
+            hc = 'rgba(0,184,0,1)'
+            cc = 'rgba(143,255,143,1)'
+            figure = self.__visualize_table(amount_of_features, self.examples_in_support, self.fact_class,
+                                            header_background_color=hc, cell_background_color=cc)
+        elif type == "examples_against":
+            hc = 'rgba(255,20,20,1)'
+            cc = 'rgba(255,170,170,1)'
+            figure = self.__visualize_table(amount_of_features, self.examples_against, self.foil_class,
+                                            header_background_color=hc, cell_background_color=cc)
+        else:
+            raise ValueError("%s not supported" % target)
+
+        self.__export(figure, target, path)
+
+    def __export(self, figure, target, path):
+        if target == "notebook":
+            self.__visualize_notebook(figure)
+        elif target == "write_to_file":
+            if path is None:
+                raise ValueError("Argument path has to be non null")
+            else:
+                self.__visualize_png(figure, path)
+        else:
+            raise ValueError("%s not supported" % target)
+
     def __sort_columns_according_to_importance(self):
         sort_index = sorted(range(len(self.coefficients)), key=lambda i: abs(self.coefficients[i]), reverse=True)
         self.feature_names = self.feature_names[sort_index]
@@ -62,10 +106,43 @@ class Explanation:
         figure = go.Figure(data=data, layout=layout)
         return figure
 
-    def visualize_feature_importance_notebook(self, amount_of_features=5):
-        figure = self.__visualize_feature_importance(amount_of_features)
+    @staticmethod
+    def __visualize_table(amount_of_features, df, class_name, header_background_color=None, cell_background_color=None):
+        if header_background_color is None:
+            header_background_color = 'rgba(0,184,0,1)'
+        if cell_background_color is None:
+            cell_background_color = 'rgba(143,255,143,1)'
+
+        header_values = Explanation.__make_bold(list(df)[:amount_of_features])
+        cell_values = df.values.transpose()[:amount_of_features]
+
+        trace = go.Table(
+            header=dict(values=header_values,
+                        fill=dict(color=header_background_color),
+                        align="center",
+                        font=dict(color='white', size=15),
+                        line=dict(color="white", width=3)),
+            cells=dict(values=cell_values,
+                       fill=dict(color=cell_background_color),
+                       align="center",
+                       font=dict(size=12),
+                       line=dict(color="white")))
+        layout = go.Layout(title="<b>Examples in support of value %s</b>" % class_name)
+
+        figure = go.Figure(data=[trace], layout=layout)
+        return figure
+
+    @staticmethod
+    def __make_bold(container):
+        return ["<b>%s</b>" % x for x in container]
+
+    @staticmethod
+    def __visualize_notebook(figure):
         py.iplot(figure)
 
-    def visualize_feature_importance_png(self, path, amount_of_features=5):
-        figure = self.__visualize_feature_importance(amount_of_features)
+    @staticmethod
+    def __visualize_png(figure, path):
         py.image.save_as(figure, filename=path)
+        print("Image saved as %s" % path)
+
+
